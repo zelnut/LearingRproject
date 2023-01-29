08d9eeb5ff981ccb4ca50a4a84a3657de4ec8eb9
Hazel <hj980830@naver.com>
2023-01-29 18:02:24 +0900

    generate in Learing R project

--- myfirstrxode.R
+++ myfirstrxode.R
@@ -1,5 +1,5 @@
 # test xode
-https://www.youtube.com/watch?v=KdpNijR0RPk
+## https://www.youtube.com/watch?v=KdpNijR0RPk
 
 
 a <- c(1:10)
@@ -7,3 +7,33 @@ b <- a * 2
 a
 b
 d <- a+b
+
+install.packages("rstan
+                 ")
+
+library("rstan")
+library(rstan)
+schools = 
+  data.frame(y = c(28., 8., -3., 7., -1., 1., 18., 12.), 
+             s =c(15., 10., 16., 11., 9., 11., 10., 18.), 
+             index=c("A","B","C","D","E","F","G", "H") )
+
+eightschools_indep = "
+data {
+  int<lower=0> J;         // number of schools 
+  real y[J];              // estimated treatment effects
+  real<lower=0> sigma[J]; // standard error of effect estimates 
+}
+parameters {
+  vector[J] theta;          // unscaled deviation from mu by school
+}
+model {
+  target += normal_lpdf(y | theta, sigma); // log-likelihood
+}
+"
+
+data = list(J = dim(schools)[1], y=schools$y, sigma = schools$s)
+
+fit.indep = 
+  stan(model_code=eightschools_indep, data=data, 
+       seed=1234567, chains=1, iter=2000, thin=10, algorithm="NUTS")
